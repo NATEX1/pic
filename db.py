04fdac2456ff166da2dff4de2267e5c55061d096
NATEX1 <nkaunkuntod.0896754371@gmail.com>
2025-12-16 15:47:07 +0700

    fixed db.py

--- db.py
+++ db.py
@@ -1,36 +1,50 @@
 import pymysql
 
-conn = pymysql.connect(
-    host="147.50.254.12",
-    user="finorfin_pic",
-    password="G5F&2!taRct9sdyv",
-    database='pic_2',
-    charset="utf8",
-    cursorclass=pymysql.cursors.DictCursor
-)
+def get_connection():
+    return pymysql.connect(
+        host="147.50.254.12",
+        user="finorfin_pic",
+        password="G5F&2!taRct9sdyv",
+        database='pic_2',
+        charset="utf8",
+        cursorclass=pymysql.cursors.DictCursor,
+        connect_timeout=60,
+        read_timeout=120,
+        write_timeout=120
+    )
 
 def fetch_one(sql, params=None):
-    cursor = conn.cursor()
-    cursor.execute(sql, params or ())
-    result = cursor.fetchone()
-    cursor.close() 
-    return result
+    conn = get_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(sql, params or ())
+            return cursor.fetchone()
+    finally:
+        conn.close()
 
 def fetch_all(sql, params=None):
-    cursor = conn.cursor()
-    cursor.execute(sql, params or ())
-    results = cursor.fetchall()
-    cursor.close() 
-    return results
+    conn = get_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(sql, params or ())
+            return cursor.fetchall()
+    finally:
+        conn.close()
 
 def execute(sql, params=None):
-    cursor = conn.cursor()
-    cursor.execute(sql, params or ())
-    conn.commit()
-    cursor.close() 
+    conn = get_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(sql, params or ())
+            conn.commit()
+    finally:
+        conn.close()
 
 def executemany(sql, params):
-    cursor = conn.cursor()
-    cursor.executemany(sql, params)
-    conn.commit()
-    cursor.close()
+    conn = get_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.executemany(sql, params)
+            conn.commit()
+    finally:
+        conn.close()
